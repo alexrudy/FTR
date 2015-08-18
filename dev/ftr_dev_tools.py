6198e39b76859cc665ba4d9f895cce387a433a19
Alex Rudy <alex.rudy@gmail.com>
2015-08-18 12:17:16 -0700

    Development tools and example notebook

---
+++ dev/ftr_dev_tools.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+#
+#  ftr_dev_tools.py
+#  FTR
+#
+#  Created by Alexander Rudy on 2015-08-12.
+#  Copyright 2015 Alexander Rudy. All rights reserved.
+#
+import numpy as np
+import numpy.fft
+
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as mgrid
+
+from FTR.utils import ignoredivide
+
+def fftmag(a):
+    """FFT magnitude."""
+    return np.real(np.abs(np.fft.fftshift(np.fft.fftn(np.asarray(a)))))
+
+def filtermag(a):
+    """Filter FFT magnitude."""
+    return np.real(np.abs(np.fft.fftshift(np.asarray(a))))
+
+def reconstruct_and_display(reconstructor, xs, ys, phi, rtol = 0.001, atol = 0.025, **kwargs):
+    """Reconstruct and display."""
+    
+    
+    phi_r = reconstructor(xs, ys)
+    display_reconstructor(phi, phi_r, **kwargs)
+    
+    with ignoredivide():
+        
+        if np.allclose(phi_r, phi, rtol=rtol, atol=atol):
+            print("Success! Filter and np.gradient match each other.")
+        else:
+            ap = reconstructor.ap
+            m_ok = (np.abs(phi_r - phi) * ap) <= atol + np.abs(phi * ap) * rtol
+            p_ok = (m_ok).sum() / np.prod(phi.shape)
+        
+            d = (np.abs(phi_r - phi) * ap)
+            d_r = (np.abs(phi_r - phi) * ap) / np.abs(phi * ap)
+            d_m = (np.abs(phi_r) / np.abs(phi))
+        
+            print("{0:.2%} of points are O.K.".format(p_ok))
+            print("rtol={0:.2g}".format(d_r[ap.astype(np.bool)].mean()))
+            print("atol={0:.2g}".format(d[ap.astype(np.bool)].mean()))
+            print("mtol={0:.2g}".format(np.nanmean(d_m[ap.astype(np.bool)])))
+    
+    return phi_r
+    
+    
+def display_reconstructor(phi, phi_r, figure=None, cmap='hot'):
+    """Reconstructor display."""
+    if figure is None:
+        figure = plt.figure()
+    
+    figure.suptitle("Phase")
+    gs = mgrid.GridSpec(2, 3)
+    
+    ax = figure.add_subplot(gs[0,0])
+    im = ax.imshow(phi, cmap=cmap)
+    ax.set_title(r"$\phi$")
+    figure.colorbar(im)
+    
+    ax = figure.add_subplot(gs[1,0])
+    im = ax.imshow(fftmag(phi), cmap=cmap)
+    ax.set_title(r"$F(\phi)$")
+    figure.colorbar(im)
+    
+    ax = figure.add_subplot(gs[0,1])
+    im = ax.imshow(phi_r, cmap=cmap)
+    ax.set_title(r"$\hat{\phi}$")
+    figure.colorbar(im)
+    
+    ax = figure.add_subplot(gs[1,1])
+    im = ax.imshow(fftmag(phi_r), cmap=cmap)
+    ax.set_title(r"$F(\hat{\phi})$")
+    figure.colorbar(im)
+    
+    ax = figure.add_subplot(gs[0,2])
+    ax.set_title(r"$\phi - \hat{\phi}$")
+    im = ax.imshow(np.abs(phi - phi_r), cmap='hot')
+    figure.colorbar(im)
+    
+    ax = figure.add_subplot(gs[1,2])
+    ax.set_title(r"$F(\phi) - F(\hat{\phi})$")
+    im = ax.imshow((fftmag(phi) - fftmag(phi_r)), cmap='hot')
+    figure.colorbar(im)
+    
+    
+    return figure
---
+++ dev/try_ftr.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from FTR import FourierTransformReconstructor
+import FTR.ftr
+from FTR.utils import circle_aperture, shapegrid, ignoredivide, complexmp
+import numpy as np
+import numpy.fft
+import argparse
+import six
+
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+
+from ftr_dev_tools import reconstruct_and_display
+    
+def make_aperture(n):
+    """Make a square aperture with a small border."""
+    ap = np.ones((n, n), dtype=np.int)
+    # ap[:2,:] = 0.0
+    # ap[:,:2] = 0.0
+    # ap[-2:,:] = 0.0
+    # ap[:,-2:] = 0.0
+    return ap
+    
+def prepare_grids(n):
+    """Prepare grids."""
+    y, x = shapegrid((n, n))
+    ap = make_aperture(n)
+    
+    # phi = np.random.randn(N, N)
+    phi = 2.0 * y **2.0 - 1.5 * x ** 2.0 + (x * -y)
+    phi -= np.mean(phi)
+    phi *= ap
+    
+    ys, xs = np.gradient(phi)
+    return ap, phi, ys, xs
+    
+def prepare_fmode_grids(n):
+    """Prepare grids in fourier space."""
+    ap = make_aperture(n)
+    phi_ft = complexmp(10.0, 2 * np.pi * np.random.random((n, n)))
+    phi_ft = (phi_ft + np.triu(phi_ft.T)).T
+    phi_ft[0,:] = 0.0
+    phi_ft[:,0] = 0.0
+    phi_ft[-1,:] = 0.0
+    phi_ft[:,-1] = 0.0
+    plt.figure()
+    plt.subplot(121)
+    plt.imshow(np.real(phi_ft))
+    phi_ft = np.fft.ifftshift(phi_ft)
+    plt.subplot(122)
+    plt.imshow(np.real(phi_ft))
+    phi_ft[0,0] = 0.0
+    phi = np.abs(np.fft.ifftn(phi_ft))
+    ys, xs = np.gradient(phi)
+    return ap, phi, ys, xs
+
+def main():
+    """Main function"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filter', default="fried", type=six.text_type,
+        choices=FourierTransformReconstructor.filters(), nargs="?",
+        help="filter name for FTR reconstructor, default='fried'")
+    parser.add_argument("--tt", help="leave tip/tilt in slopes",
+         action='store_false', dest='manage_tt')
+    parser.add_argument("-n", type=int, default=20,
+        help="number of subapertures on a side.")
+    opt = parser.parse_args()
+    ap, phi, ys, xs = prepare_fmode_grids(opt.n)
+    recon = FourierTransformReconstructor(ap, opt.filter,
+        manage_tt=opt.manage_tt, suppress_tt=False)
+    print(recon)
+    xs, ys = recon.invert(phi)
+    phi_r = reconstruct_and_display(recon, xs, ys, phi)
+        
+    plt.show()
+    
+if __name__ == '__main__':
+    main()
