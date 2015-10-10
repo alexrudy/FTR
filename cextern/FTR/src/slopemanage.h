9c0db5c2462ea8c49b784721fa63a9bc9ac111c3
Alex Rudy <alex.rudy@gmail.com>
2015-10-10 16:23:02 -0700

    Adding an implementation of slopemanagement

---
+++ cextern/FTR/src/slopemanage.h
@@ -0,0 +1,20 @@
+//
+//  slopemanage.h
+//  FTR
+//
+//  Created by Alexander Rudy on 2015-10-10.
+//  Copyright 2015 Alexander Rudy. All rights reserved.
+//
+
+typedef struct slope_management_plan_s {
+    int nx;
+    int ny;
+    int *row_any, *left, *right;
+    int *col_any, *top, *bottom;
+    double *y_row_sum, *x_col_sum;
+    int *ap;
+} * sm_plan;
+
+sm_plan slope_management_plan(int nx, int ny, int *ap);
+void slope_management_execute(sm_plan plan, double * sx, double * sy);
+void slope_management(int nx, int ny, int *ap, double * sx, double * sy);
