a2e3d658db11ad35abc062c0a044f144fab18b0f
Alex Rudy <alex.rudy@gmail.com>
2015-10-14 23:45:26 -0400

    Python bindings for slope management exposed.

--- FTR/libftr/_slopemanage.pyx
+++ FTR/libftr/_slopemanage.pyx
@@ -23,15 +23,44 @@ cdef extern from "slopemanage.h":
 #Need to ensure that we've initialized the numpy c-API.
 np.import_array()
 
-def do_slope_management(np.ndarray[np.int_t, ndim=2] ap, np.ndarray[np.double_t, ndim=2] sy, np.ndarray[np.double_t, ndim=2] sx):
+def slope_management_fast(np.ndarray[np.int_t, ndim=2] ap, np.ndarray[np.double_t, ndim=2] sy, np.ndarray[np.double_t, ndim=2] sx):
     """Slope management done in a single function call, including memory allocation, etc.
     
     This function acts in-place on sx and sy.
     """
-    
     cdef int nx, ny
     ny = ap.shape[0]
     nx = ap.shape[1]
-    _ap = ap.astype(np.int32)
-    slope_management(ny, nx, <int *>np.PyArray_DATA(_ap), <double *>np.PyArray_DATA(sy), <double *>np.PyArray_DATA(sx))
+    slope_management(ny, nx, <int *>np.PyArray_DATA(ap.astype(np.int32)), <double *>np.PyArray_DATA(sy), <double *>np.PyArray_DATA(sx))
     return
+
+cdef class SlopeManager:
+    """A class to manage memory allocated for slope management on a specific aperture."""
+    
+    cdef sm_plan _plan
+    cdef tuple _shape
+    
+    def __cinit__(self, ap):
+        """C initialization step for this function."""
+        shape = np.asarray(ap).shape
+        self._shape = shape
+        
+        # Create the plan, which internally allocates the necessary memory.
+        self._plan = slope_management_plan(shape[0], shape[1], <int *>np.PyArray_DATA(ap.astype(np.int32)))
+        
+    def __dealloc__(self):
+        slope_management_destroy(self._plan)
+        
+    def __call__(self, np.ndarray[np.double_t, ndim=2] sy, np.ndarray[np.double_t, ndim=2] sx):
+        """Perform the actual slope management.
+        
+        The slope management happens in-place in the array.
+        
+        :parameter np.ndarray sy: The y slopes.
+        :parameter np.ndarray sx: The x slopes.
+        
+        This function does not return anything because all work happens in place.
+        """
+        slope_management_execute(self._plan, <double *>np.PyArray_DATA(sy), <double *>np.PyArray_DATA(sx))
+        return None
+        
