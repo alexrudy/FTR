2f56099bbeac6df8d35560e6d4140be233af0874
Alex Rudy <alex.rudy@gmail.com>
2017-01-19 15:34:54 -0800

    Comparison tests for LQG filters

--- FTR/tests/test_lqg.py
+++ FTR/tests/test_lqg.py
@@ -21,14 +21,31 @@ class TestLQGFilter(FilterTestBase):
     def nlayers(self, request):
         """Return the number of layers."""
         return request.param
+        
+    @pytest.fixture
+    def full_filter_shape(self, nlayers, shape):
+        """Shape of the filter, accounting for number of layers."""
+        return (nlayers,) + shape
+        
+    @pytest.fixture
+    def gains(self, full_filter_shape):
+        """Gains"""
+        return np.random.randn(*full_filter_shape) + 1j * np.random.randn(*full_filter_shape)
+        
+    @pytest.fixture
+    def alphas(self, full_filter_shape):
+        """Alphas"""
+        return np.random.randn(*full_filter_shape) + 1j * np.random.randn(*full_filter_shape)
+        
+        
+    @pytest.fixture
+    def hp_coeffs(self, shape):
+        """High-pass coefficients."""
+        return np.random.randn(*shape) + 1j * np.random.randn(*shape)
     
     @pytest.fixture
-    def filter(self, nlayers, shape):
+    def filter(self, gains, alphas, hp_coeffs):
         """Generate a random filter for testing."""
-        full_shape = (nlayers,) + shape
-        gains = np.random.randn(*full_shape) + 1j * np.random.randn(*full_shape)
-        alphas = np.random.randn(*full_shape) + 1j * np.random.randn(*full_shape)
-        hp_coeffs = np.random.randn(*shape) + 1j * np.random.randn(*shape)
         return self.cls(gains, alphas, hp_coeffs)
         
     
@@ -39,4 +56,26 @@ class TestCLQG(TestLQGFilter):
     cls = FastLQGFilter
     
     repr = "<FastLQGFilter nlayers=... shape={shape}>"
+    
+    @pytest.fixture
+    def filter_py(self, gains, alphas, hp_coeffs):
+        """Pure-python LQG filter"""
+        return LQGFilter(gains, alphas, hp_coeffs)
+    
+    def test_compare_to_python(self, filter, filter_py, phase_ft):
+        """docstring for test_compare_to_python"""
+        np.testing.assert_allclose(filter.gains, filter_py.gains)
+        np.testing.assert_allclose(filter.alphas, filter_py.alphas)
+        np.testing.assert_allclose(filter.highpass_coefficients, filter_py.highpass_coefficients)
+        
+        filter.reset()
+        phase_c1 = filter(phase_ft)
+        phase_c2 = filter(phase_ft)
+        
+        filter_py.reset()
+        phase_py1 = filter_py(phase_ft)
+        phase_py2 = filter_py(phase_ft)
+        
+        np.testing.assert_allclose(phase_c1, phase_py1, atol=1e-7)
+        np.testing.assert_allclose(phase_c2, phase_py2, atol=1e-7)
     
