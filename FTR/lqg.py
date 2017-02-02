1ac999e95351d453fbbf9b9ae87dbfa31dd738fa
Alex Rudy <alex.rudy@gmail.com>
2017-02-01 17:17:37 -0800

    Fix a bug in HDF5 reading for LQG

--- FTR/lqg.py
+++ FTR/lqg.py
@@ -86,7 +86,7 @@ class LQGBase(Filter, IOBase):
         """Read an LQG filter from an HDF5 file."""
         import h5py
         with h5py.File(file, kwargs.pop('mode', 'r')) as file:
-            group = file.get('LQG Filter', next(file.values()))
+            group = file.get('LQG Filter', next(iter(file.values())))
             args = [group[name][...] for name in [ "gains", "alphas", "highpass_coefficients" ]]
         return cls(*args)
         
